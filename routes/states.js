1782ea2af65e4d57c2f95c5f3777f79376481e04
Jordyn Bredenberg <jordynbredenberg@Jordyns-MacBook-Pro-4.local>
2025-05-10 17:01:26 -0500

    Fix route order so specific routes are matched before :state

--- routes/states.js
+++ routes/states.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const controller = require('../controllers/statesController');
 
 router.get('/', controller.getAllStates);
-router.get('/:state', controller.getState);
+
+
 router.get('/:state/funfact', controller.getRandomFunFact);
 router.get('/:state/capital', controller.getCapital);
 router.get('/:state/nickname', controller.getNickname);
 router.get('/:state/population', controller.getPopulation);
 router.get('/:state/admission', controller.getAdmission);
+router.get('/:state', controller.getState);
 
 router.post('/:state/funfact', controller.postFunFacts);
 router.patch('/:state/funfact', controller.patchFunFact);
